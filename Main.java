f94b566ad73d8850d903af687a907f4e4cb686e2
Amir <fdhjdzcb@gmail.com>
2022-11-20 15:48:59 +0300

    Шаблоны в Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main() {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        // Builder / строитель
+        films film = new films();
+        film.setName("Аватар 2");
+        film.setGenre("Фэнтези");
+        film.setCountry("США");
+        film.setAgeLimit(16);
+        System.out.println(film.getName());
+        System.out.println(film.getGenre());
+        System.out.println(film.getCountry());
+
+
+        // Iterator / итератор
+        List<String> data = Arrays.asList("Чужой", "Ярость", "Ирония судьбы");
+        Iterator<String> iterator = data.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
